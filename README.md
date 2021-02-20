bedfe44ada636ff8cdd522e5a6cfcad328bcb437
Dylan Ross <dylanross620@gmail.com>
2021-02-20 14:13:35 -0500

    Changed to using google sheets for quote saving

--- README.md
+++ README.md
@@ -13,5 +13,13 @@ In addition to setting up the Twitch token, you also must open the file ```bot.p
 - `NAME` is the name of the account the bot will be posting from. This should match the name of the account you retrieved your token with.
 - `OWNER` is the Twitch chat that the bot will run in.
 
+### Google Sheets Setup
+Firstly, go to https://developers.google.com/sheets/api/quickstart/python#step_1_turn_on_the and click the button labeled "Enable the Google Sheets API". Enter a project name, then select Desktop app from the dropdown menu. Press "DOWNLOAD CLIENT CONFIGURATION" and save the
+resulting `credentials.json` file to the project directory.
+Next, create a spreadsheet for the quotes to be placed in and generate a shared link where people with the link can view the spreadsheet. Then fill in the following variables in ```bot,py```:
+- `SPREADSHEET_ID` is the id of the spreadsheet you made it can be found in the url as `docs.google.com/spreadsheets/d/<your_id_here>/edit`.
+- `RANGE_NAME` should be in the form of `<sheet 1 name>!A:A`.
+- `READ_LINK` should be the shareable viewing link you created above.
+
 ## Running
 To run the bot, perform setup if you have not already. Once setup is completed run ```python bot.py```
--- bot.py
+++ bot.py
@@ -3,12 +3,28 @@ import json
 from datetime import date
 import random
 
-NAME = 'VantchkoBot'
-OWNER = 'Vantchko'
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+NAME = 'Radnor0'
+OWNER = 'Radnor0'
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# The ID and range of a sample spreadsheet.
+SPREADSHEET_ID = '116l4KSU_G22t_tKIw6AE62gLOgkVUaam1uSHval62kE'
+RANGE_NAME = 'Quotes!A:A'
+READ_LINK = 'https://docs.google.com/spreadsheets/d/116l4KSU_G22t_tKIw6AE62gLOgkVUaam1uSHval62kE/edit?usp=sharing'
 
 class TwitchBot(irc.bot.SingleServerIRCBot):
-    def __init__(self, username, token, channel, quote_list):
+    def __init__(self, username, token, channel, quote_list, sheet):
         self.quote_list = quote_list
+        self.sheet = sheet
+
         if token[:6] != 'oauth:':
             token = 'oauth:' + token
         self.channel = '#' + channel
@@ -51,8 +67,14 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
                 self.do_command(e, message_words[1].lower(), message_words[2:], tags)
 
     def save_quotes(self):
-        with open('quotes.json', 'w') as f:
-            f.write(json.dumps({'quotes': self.quote_list}))
+        # Clear sheet to remove old quotes that may have been removed
+        self.sheet.values().clear(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+
+        # Set the sheet to have the new values
+        body = {'range':RANGE_NAME,
+                'values':[[q] for q in self.quote_list],
+                'majorDimension':'ROWS'}
+        self.sheet.values().update(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME, body=body, valueInputOption='USER_ENTERED').execute()
 
     def do_command(self, e, cmd, args, tags):
         c = self.connection
@@ -80,6 +102,9 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
         # Check if asking to count quotes
         if cmd == 'count':
             self.send_message(f"{tags['display-name']} There are {len(self.quote_list)} quotes")
+            return
+        if cmd == 'list':
+            self.send_message(f"{tags['display-name']} The quote list can be found at {READ_LINK}")
             return
 
         # All future commands require at least 1 argument, so ensure it is there
@@ -118,28 +143,41 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
 def start():
     # Try to load token and client_id from 'token.env' file
     token = None
-    with open('token.env', 'r') as f:
-        token = f.readline().strip()
-    assert token is not None, 'Error loading twitch token and client id from twitch_token.env'
+    if os.path.exists('token.env'):
+        with open('token.env', 'r') as f:
+            token = f.readline().strip()
+    if not token:
+        token = input('Please enter your Twitch TMI token: ')
+        with open('token.env', 'w') as f:
+            f.write(token)
 
-    # Try to load existing quotes list from 'quotes.json' file
-    quote_list = []
-    try:
-        with open('quotes.json', 'r') as f:
-            json_str = f.readline().strip()
-            quote_list = json.loads(json_str)['quotes']
-    except:
-        pass
-    
-    if len(quote_list) == 0:
-        print('Unable to load quotes, initializing new list')
-    else:
-        print('Successfully loaded quotes list')
+    # Try to connect to the Google Sheets API
+    # load credentials
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save credentials
+        with open('token.json', 'w') as f:
+            f.write(creds.to_json())
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    sheet = service.spreadsheets()
+
+    # Try to load existing quotes list from sheet
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+    quote_list = [res[0] for res in result.get('values', [])]
 
     username = NAME
     channel = OWNER.lower()
 
-    bot = TwitchBot(username, token, channel, quote_list)
+    bot = TwitchBot(username, token, channel, quote_list, sheet)
     bot.start()
 
 if __name__ == '__main__':
