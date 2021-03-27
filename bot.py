a40624ed316eedaa57620824021f325792abcda3
Dylan Ross <dylanross620@gmail.com>
2021-03-26 23:00:54 -0400

    Added Kappa

--- bot.py
+++ bot.py
@@ -224,7 +224,7 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
 
         # Easter egg to mess with Del
         if dice_type == 1:
-            self.send_message(f"Nice try, Del")
+            self.send_message(f"Nice try, Del Kappa")
             return
 
         # Roll the dice
